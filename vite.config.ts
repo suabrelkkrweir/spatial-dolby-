ab42185ffd3b463bd38339311b4d06c0d2f0011e
suabrelkkrweir <subham827k@gmail.com>
2026-09-21 16:06:29 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  root: './',
   plugins: [react(), tailwindcss()],
   base: './',
   resolve: {
